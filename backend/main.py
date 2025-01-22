ab40003492fd0d80bb4a28b1bf2e7eb0ea0dd6bb
Marek Wannepain <marekwannepain@gmail.com>
2025-01-22 06:57:15 +0100

    finish respond route

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,25 @@
-from flask import Flask
+from flask import Flask, json
 from flask import request
 from chatbot import respond
-from corpus import small_corpus
+from corpus import inicialize_small_corpus, inicialize_medium_corpus
 app = Flask(__name__)
-@app.route("/")
+
+history = [] #this will eitherbe stored on the client side (local sotrage) or db
+corpus = inicialize_medium_corpus()
+used_question_idx = []
+
+@app.route("/respond")
 def home():
-    latest_response = request.args.get("res")
-    print(latest_response)
-    #implement response processing funcionalities inside this code
-    return "<h1>Flask is working</h1>"
+    client_response = request.args.get("res")
+
+    if client_response and history:
+        print(client_response)
+        # Update the last entry in the history with the client's response
+        history[-1]["client"] = client_response
+
+    return_response = respond(history, corpus, used_question_idx)
+    print(return_response)
+    
+    return json.dumps({"response":history[-1]["bot"]["Question_Text"], "history":history})
    
 app.run()
