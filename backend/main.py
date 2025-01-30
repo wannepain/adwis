dcaf8df3b3dfaafc2265435abfcc7c2e06044e28
Marek Wannepain <marekwannepain@gmail.com>
2025-01-30 17:56:13 +0100

    initialize frontend with Vite and React setup

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,27 @@
 from flask import Flask, json
 from flask import request
+from flask_cors import CORS, cross_origin
 from chatbot import respond
 from corpus import inicialize_medium_corpus  # Corrected import
 from career import return_career
 from evaluation import evaluate
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
 
 corpus = inicialize_medium_corpus()
 used_question_idx = []
 
 
+@app.route("/", methods=["GET"])
+@cross_origin()
+def home():
+    return "Hello, World!"
+
+
 @app.route("/respond", methods=["POST"])
-def respond_route():
+def respond_route():  # need to move the used question idx to the global scope
     request_data = request.get_json()
     history_in_req = request_data["history"]
 
